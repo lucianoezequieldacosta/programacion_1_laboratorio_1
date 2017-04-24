4ac7883fd44cd4503a88320dbb586eebc75a618d
lucianoezequieldacosta <lu_taladro@hotmail.com>
2017-04-24 09:28:06 -0300

    Add files via upload

---
+++ Clase8/main.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct
+{
+    int id;
+    char nombre[20];
+    int categoria;
+    float sueldo;
+
+}eProgramador;
+
+typedef struct
+{
+    int idCategoria;
+    char descripcion[20];
+}eCategoria;
+
+
+int main()
+{
+
+}
